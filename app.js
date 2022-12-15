94cb264ef541f338296876cb1d32344730843c6a
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-15 21:01:20 +0000

    ejs tools for layout

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @ Author: Abdou Lahi DIOP
  * @ Create Time: 2022-12-12 00:34:06
  * @ Modified by: Abdou Lahi DIOP
- * @ Modified time: 2022-12-13 13:27:31
+ * @ Modified time: 2022-12-15 20:46:05
  * @ Description:
  */
 
@@ -10,9 +10,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const campground = require('./models/campground');
+// const campground = require('./models/campground');
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
@@ -26,6 +27,7 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
