60056f26e52485390b5c6ef7af35d688bca67f84
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-26 21:00:03 +0000

    Joi validation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @ Author: Abdou Lahi DIOP
  * @ Create Time: 2022-12-12 00:34:06
  * @ Modified by: Abdou Lahi DIOP
- * @ Modified time: 2022-12-26 16:07:20
+ * @ Modified time: 2022-12-26 20:59:22
  * @ Description:
  */
 
@@ -11,10 +11,12 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { required } = require('joi');
 // const campground = require('./models/campground');
 
 mongoose.set('strictQuery', false);
@@ -50,7 +52,22 @@ app.get('/campgrounds/new', async (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Camground Data', 400);
+    const campgrounSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    });
+    const { error } = campgrounSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    }
+    console.log(result);
+    // if (!req.body.campground) throw new ExpressError('Invalid Camground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
